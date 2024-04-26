87955be387d6d7c8a54edafb309c3810e912e30d
Sri-Varshini-Sukumar-12 <167115224+Sri-Varshini-Sukumar-12@users.noreply.github.com>
2024-04-26 16:49:25 +0530

    Add files via upload

---
+++ String_Length_Pattern.c
@@ -0,0 +1,51 @@
+/*The program must accept two string values S1 nd S2. The program must print
+the pattern as shown in the input/output sections.
+
+Boundary Conditions
+1 <= Length of S1,S2 <= 100
+
+Example Input/Output 1:
+Input:
+bad water
+
+Output:
+bw
+aa
+dt
+*e
+*r
+
+Input2:
+gionee kkr
+
+Outpur:
+gk
+ik
+or
+n*
+e*
+e*
+
+*/
+#include<stdio.h>
+#include<string.h>
+
+int main() {
+    char S1[101], S2[101];
+    scanf("%s %s", S1, S2);
+    int l1 = strlen(S1);
+    int l2 = strlen(S2);
+    char ch = '*';
+    int len = (l1 > l2) ? l1 : l2; // Find the maximum length
+    
+    for (int i = 0; i < len; i++) {
+        if (i < l1 && i < l2) {
+            printf("%c%c\n", S1[i], S2[i]);
+        } else if (i < l1) {
+            printf("%c%c\n", S1[i], ch);
+        } else if (i < l2) {
+            printf("%c%c\n", ch, S2[i]);
+        }
+    }
+    return 0;
+}
